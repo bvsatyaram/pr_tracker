6ca12d8d3a5e788a39eed5c14667c941ee486878
B V Satyaram <bvsatyaram@gmail.com>
2015-10-19 14:27:54 +0530

    Generate User model using devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019083937) do
+ActiveRecord::Schema.define(version: 20151019085229) do
 
   create_table "pull_requests", force: :cascade do |t|
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
