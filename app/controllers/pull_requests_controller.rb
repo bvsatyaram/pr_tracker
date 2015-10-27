b0a1755d93cf2cf3220ab25987d0a977d6cca8d5
B V Satyaram <bvsatyaram@gmail.com>
2015-10-27 13:15:38 +0530

    Get PR details

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -15,10 +15,19 @@ class PullRequestsController < ApplicationController
 
   def show
     @pull_request = PullRequest.find(params[:id])
+    user_name, repo_name, pr_number = fetch_pr_details(@pull_request)
+    github = Github.new
+    github_prs = github.pulls.list user: "CodeAstra", repo: "WereWolf"
+    @pr_object = github_prs.select{|pr| pr.number == pr_number.to_i}.first
   end
 
 private
   def pull_request_params
     params.require(:pull_request).permit(:link)
+  end
+
+  def fetch_pr_details(pull_request)
+    details = pull_request.link.split("github.com")[1].split("/")
+    return [details[1], details[2], details[4]]
   end
 end
